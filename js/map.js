17e96f8e25b119364449ff98903978e2e0a8be1e
Merrick Richers <merrickrichers@gmail.com>
2024-03-14 19:36:52 -0700

    Added basic popup menus

--- js/map.js
+++ js/map.js
@@ -28,12 +28,28 @@ var GreenWaveIcon = L.Icon.extend({
  // Adding layer to the map
  map.addLayer(layer)
 
+
+// create popup contents, should be a string with template literals embedded into it in the future...
+// this implementation is terrible.
+var aurorasolar = "<div><a style='text-align: center;' href = https://aurorasolar.com/> Aurora Solar </a> <hr> <p> Provides software suite for simplifying the site assesment and installation design process.</p> <hr> <p>Cost: $159-259</p> <hr> <p> <i class='tiny material-icons'>star</i> <i class='tiny material-icons'>star</i> <i class='tiny material-icons'>star</i> <i class='tiny material-icons'>star</i> <i class='tiny material-icons'>star_half</i> </p> </div>";
+var ecosolar = "<div><a style='text-align: center;' href = http://www.ecosolarenergy.net/> Eco Solar Energy </a> <hr> <p> Nicaraguan residential solar intaller. </p> <hr> <p>Cost: Requires Solicitation. </p> <hr> <p> <i class='tiny material-icons'>star</i> <i class='tiny material-icons'>star</i> <i class='tiny material-icons'>star</i> <i class='tiny material-icons'>star_half</i> <i class='tiny material-icons'>star_border</i> </p> </div>";
+// specify popup options 
+var customOptions =
+   {
+   'maxWidth': '150',
+   'className' : 'custom'
+   
+   }
+ 
+ // creates the markers, needs to support database storage in the future, this sucks!
  var marker = L.marker([37.78955930049995, -122.40403373160581], {icon: supplierIcon})
  .addTo(map)
- .bindPopup("Aurora Solar")
+ .bindPopup(aurorasolar, customOptions)
 
  var marker = L.marker([12.103539004571969, -86.26922537500955], {icon: installerIcon})
  .addTo(map)
- .bindPopup("Censolar Nicaragua")
+ .bindPopup(ecosolar, customOptions)
+
+ 
 
  window.onload = window.onresize = function(){map.invalidateSize();};
